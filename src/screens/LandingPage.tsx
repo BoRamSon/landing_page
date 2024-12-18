71f0425de734fabd011123de69cd61b7210aab0b
BoRamSon <onbrson1@gmail.com>
2024-12-19 03:55:02 +0900

    feat(screens): change grid shape Landing page

--- src/screens/LandingPage.tsx
+++ src/screens/LandingPage.tsx
@@ -1,8 +1,15 @@
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid';
+import '../styles/LandingPage.css';
+import mainmap from '../assets/images/MainMap.png';
+import urlogLogo from '../assets/images/URLog_Logo.png';
+// import machine from '../assets/images/세탁기.png';
 
 const LandingPage: React.FC = () => {
   return (
-    <div className='relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0'>
+    <div className='relative isolate overflow-hidden bg-white px-6 py-10 lg:overflow-visible lg:px-0'>
+      {/* =================================================================================================================================== 
+          Background 
+      =================================================================================================================================== */}
       <div className='absolute inset-0 -z-10 overflow-hidden'>
         <svg
           aria-hidden='true'
@@ -28,44 +35,109 @@ const LandingPage: React.FC = () => {
           </svg>
           <rect
             fill='url(#e813992c-7d03-4cc4-a2bd-151760b470a0)'
-            width='100%'
-            height='100%'
+            width='100%' // 화면 전체를 채우도록 설정
+            height='100%' // 화면 전체를 채우도록 설정
             strokeWidth={0}
           />
         </svg>
       </div>
-      <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10'>
+
+      {/* =================================================================================================================================== 
+          Body start  |||   grid setting - grid-cols-1 grid-row-1  /  lg:grid-cols-2 lg:grid-row-6
+      =================================================================================================================================== */}
+      <div className='mx-auto grid max-w-2xl grid-cols-1 grid-row-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:grid-row-6 lg:items-start lg:gap-y-10'>
         <div className='lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8'>
           <div className='lg:pr-4'>
             <div className='lg:max-w-lg'>
-              <p className='text-base/7 font-semibold text-indigo-600'>Deploy faster</p>
-              <h1 className='mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
-                A better workflow
-              </h1>
-              <p className='mt-6 text-xl/8 text-gray-700'>
-                Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui
-                mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.
+              <p className='text-base/7 font-semibold text-indigo-600'>
+                발자국을 따라가며 여정을 기록해드리겠습니다.
+              </p>
+              {/* <h1 className='mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
+                URLog
+              </h1> */}
+              <div className='flex justify-center items-center lg:flex-1'>
+                <a href='#' className='-m-0.5'>
+                  <span className='sr-only'>Your Company</span>
+                  <img alt='' src={urlogLogo} className='h-20 w-auto' />
+                </a>
+              </div>
+              <p className='mt-2 text-xl/8 text-gray-700'>
+                서비스 한줄 소개 <br />
+                00000000 Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu,
+                sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.
               </p>
             </div>
           </div>
         </div>
-        <div className='-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden'>
+
+        {/* =================================================================================================================================== 
+          image 1 (Main)  |||   grid prossess - lg:row-span-1 lg:row-start-1 lg:col-span-1 lg:col-start-2 
+        =================================================================================================================================== */}
+        <div className='-mt-6 p-6 lg:sticky lg:top-4 lg:row-span-1 lg:row-start-1 lg:col-span-1 lg:col-start-2 lg:overflow-hidden flex justify-center items-center'>
+          {/* <div className='-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden'> */}
           <img
-            alt=''
-            // src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot."
-            className='w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]'
+            alt='MainMap'
+            src={mainmap}
+            className='w-[15rem] max-w-none rounded-xl sm:w-[18rem] md:w-[20rem] lg:w-[25rem] xl:w-[30rem]' // 실직적인 이미지 크기 수정
+            // className='w-[38rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]'
           />
         </div>
-        <div className='lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8'>
+
+        {/* =================================================================================================================================== 
+          ************************** Explanation for my product *******************************
+          explanation 1  |||   grid prossess - lg:row-span-1 lg:row-start-2 lg:col-span-1 lg:col-start-1 
+        =================================================================================================================================== */}
+        <div className='lg:row-span-1 lg:row-start-2 lg:col-span-1 lg:col-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8 flex justify-center items-center'>
           <div className='lg:pr-4'>
             <div className='max-w-xl text-base/7 text-gray-700 lg:max-w-lg'>
               <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis
-                mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio
+                111111 Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.
+                Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio
                 penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa
                 rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
                 vitae sed turpis id.
               </p>
+            </div>
+          </div>
+        </div>
+
+        {/* =================================================================================================================================== 
+          image 2  |||   grid prossess - lg:row-span-1 lg:row-start-2 lg:col-span-1 lg:col-start-2 
+        =================================================================================================================================== */}
+        <div className='-mt-6 p-6 lg:sticky lg:top-4 lg:row-span-1 lg:row-start-2 lg:col-span-1 lg:col-start-2 lg:overflow-hidden flex justify-center items-center'>
+          <img
+            alt='Photo'
+            src={mainmap}
+            className='w-[15rem] max-w-none rounded-xl sm:w-[18rem] md:w-[20rem] lg:w-[25rem] xl:w-[30rem]'
+          />
+        </div>
+
+        {/* =================================================================================================================================== 
+          image 3  |||   grid prossess - lg:row-span-1 lg:row-start-3 lg:col-span-1 lg:col-start-2 
+        =================================================================================================================================== */}
+        <div className='-mt-6 p-6 lg:sticky lg:top-4 lg:row-span-1 lg:row-start-3 lg:col-span-1 lg:col-start-2 lg:overflow-hidden flex justify-center items-center'>
+          <img
+            alt='Photo'
+            src={mainmap}
+            className='w-[15rem] max-w-none rounded-xl sm:w-[18rem] md:w-[20rem] lg:w-[25rem] xl:w-[30rem]'
+          />
+        </div>
+
+        {/* =================================================================================================================================== 
+          explanation 2  |||   grid prossess - lg:row-span-1 lg:row-start-3 lg:col-span-1 lg:col-start-1 
+        =================================================================================================================================== */}
+        <div className='lg:row-span-1 lg:row-start-3 lg:col-span-1 lg:col-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8 flex justify-center items-center'>
+          <div className='lg:pr-4'>
+            <div className='max-w-xl text-base/7 text-gray-700 lg:max-w-lg'>
+              <div className='flex justify-center items-center'>
+                <CloudArrowUpIcon
+                  aria-hidden='true'
+                  className='mt-16 size-5 flex-none text-indigo-600'
+                />
+                <span className='mt-16 text-2xl font-bold tracking-tight text-gray-1000'>
+                  핵심기능 <br />
+                </span>
+              </div>
               <ul role='list' className='mt-8 space-y-8 text-gray-600'>
                 <li className='flex gap-x-3'>
                   <CloudArrowUpIcon
@@ -73,9 +145,9 @@ const LandingPage: React.FC = () => {
                     className='mt-1 size-5 flex-none text-indigo-600'
                   />
                   <span>
-                    <strong className='font-semibold text-gray-900'>Push to deploy.</strong> Lorem
-                    ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
-                    suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                    <strong className='font-semibold text-gray-900'>Push to deploy</strong>
+                    <br /> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit
+                    perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
                   </span>
                 </li>
                 <li className='flex gap-x-3'>
@@ -101,12 +173,32 @@ const LandingPage: React.FC = () => {
                   </span>
                 </li>
               </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* =================================================================================================================================== 
+          explanation 3  |||   grid prossess - lg:row-span-1 lg:row-start-5 lg:col-span-1 lg:col-start-1 
+        =================================================================================================================================== */}
+        <div className='lg:row-span-1 lg:row-start-4 lg:col-span-1 lg:col-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8 flex justify-center items-center'>
+          <div className='lg:pr-4'>
+            <div className='max-w-xl text-base/7 text-gray-700 lg:max-w-lg'>
               <p className='mt-8'>
                 Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id
                 blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel.
                 Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac
                 tempor et ut. Ac lorem vel integer orci.
               </p>
+            </div>
+          </div>
+        </div>
+
+        {/* =================================================================================================================================== 
+          explanation 4  |||   grid prossess - lg:row-span-1 lg:row-start-6 lg:col-span-1 lg:col-start-1 
+        =================================================================================================================================== */}
+        <div className='lg:row-span-1 lg:row-start-5 lg:col-span-1 lg:col-start-1  lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:gap-x-8 lg:px-8 flex justify-center items-center'>
+          <div className='lg:pr-4'>
+            <div className='max-w-xl text-base/7 text-gray-700 lg:max-w-lg'>
               <h2 className='mt-16 text-2xl font-bold tracking-tight text-gray-900'>
                 No server? No problem.
               </h2>
