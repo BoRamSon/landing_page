6932b8d9471cf39fd5a55015b2546a38385d1f81
BoRamSon <onbrson1@gmail.com>
2024-12-18 16:52:58 +0900

    feat(components): create DownloadButton.tsx

---
+++ src/components/ButtonDownload.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import '../styles/component/DownloadButton.css';
+
+const DownloadButton: React.FC = () => {
+  return (
+    <div className='buttons'>
+      <button type='button' className='btn btn-android'>
+        Android GoogleStroe
+      </button>
+      <button type='button' className='btn btn-iOS'>
+        iOS AppStore
+      </button>
+
+      {/* <a className='btn btn-android' href='#' role='button'>
+        Android GoogleStroe
+      </a>
+      <a className='btn btn-iOS' href='#' role='button'>
+        iOS AppStore
+      </a> */}
+    </div>
+  );
+};
+
+export default DownloadButton;
