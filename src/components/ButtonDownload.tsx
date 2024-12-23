64a3855f68e6cf6da225196cfaf5b3fa4a9400a1
BoRamSon <onbrson1@gmail.com>
2024-12-23 20:01:45 +0900

    feat(component): update download button

--- src/components/ButtonDownload.tsx
+++ src/components/ButtonDownload.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import '../styles/component/DownloadButton.css';
+import '../styles/component/ButtonDownload.css';
+import { useNavigate } from 'react-router-dom';
+import { ArrowDownCircleIcon } from '@heroicons/react/20/solid';
 
 const DownloadButton: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='buttons'>
-      {/* <button type='button' className='btn btn-android'>
-        Android GoogleStroe
+      <button type='button' className='btn btn-android' onClick={() => navigate('/notice')}>
+        <ArrowDownCircleIcon className='store-icon' />
+        <div className='btn-text'>
+          <span className='btn-subtitle'>GET IT ON</span>
+          <span className='btn-title'>Google Play</span>
+        </div>
       </button>
-      <button type='button' className='btn btn-iOS'>
-        iOS AppStore
-      </button> */}
-
-      <a className='btn btn-android' href='#' role='button'>
-        Android GoogleStroe
-      </a>
-      <a className='btn btn-iOS' href='#' role='button'>
-        iOS AppStore
-      </a>
+      <button type='button' className='btn btn-iOS' onClick={() => navigate('/notice')}>
+        <ArrowDownCircleIcon className='store-icon' />
+        <div className='btn-text'>
+          <span className='btn-subtitle'>Download on the</span>
+          <span className='btn-title'>App Store</span>
+        </div>
+      </button>
     </div>
   );
 };
