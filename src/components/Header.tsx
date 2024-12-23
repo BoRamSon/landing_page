81bcc99e6db97b602eca6b0bd8d77dc14b224a44
BoRamSon <onbrson1@gmail.com>
2024-12-24 02:59:42 +0900

    feat(componenet): add DownloadButton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,25 @@
 import urlogLogo from '../assets/images/URLog_Logo.png';
-import DownloadButton from './ButtonDownload.tsx';
+import DownloadButton from './DownloadButton.tsx';
 
 const Header = () => {
   return (
-    <header className='bg-white'>
-      <nav
-        aria-label='Global'
-        className='mx-auto flex max-w-7xl items-center justify-between p-0 lg:px-0'
-      >
-        <div className='flex lg:flex-1'>
-          <a href='#' className='-m-1.5 p-1.5'>
-            <span className='sr-only'>Your Company</span>
-            <img alt='logo' src={urlogLogo} className='h-20 w-auto' />
-          </a>
-        </div>
+    <>
+      <header className='fixed top-0 left-0 w-full bg-white z-10 shadow h-18'>
+        <nav
+          aria-label='Global'
+          className='mx-auto flex max-w-7xl items-center justify-between p-0 lg:px-0'
+        >
+          <div className='flex lg:flex-1'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img alt='logo' src={urlogLogo} className='h-16 w-auto' />
+            </a>
+          </div>
 
-        <DownloadButton />
-      </nav>
-    </header>
+          <DownloadButton />
+        </nav>
+      </header>
+    </>
   );
 };
 
