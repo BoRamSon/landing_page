aa85f9c9aa8aa218551b7acf13b8d444783fef70
BoRamSon <onbrson1@gmail.com>
2025-01-15 22:49:35 +0900

    feat(navigation): add code 'GA4 Initialize' and 'GTM Initialize'

--- src/navigations/RootNavigator.tsx
+++ src/navigations/RootNavigator.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4'; // 1. GA 관련 import 추가
+import TagManager from 'react-gtm-module'; // 4. GTM 관련 import 추가
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from '../components/Header.tsx';
 // import Sidebar from '../components/Sidebar.tsx';
 // import Button from '../components/ButtonBasic.tsx';
@@ -11,9 +13,37 @@ import NoticePage from '../screens/NoticePage.tsx';
 import Footer from '../components/Footer.tsx';
 import '../styles/navigation/RootNav.css';
 
+// 3. 페이지뷰 전송
+const TrackPageView = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    // 해시 URL 변경 시 페이지뷰 전송
+    ReactGA.send({
+      hitType: 'pageview',
+      page: location.pathname + location.search + location.hash,
+    });
+  }, [location]);
+
+  return null;
+};
+
 const RootNavigator: React.FC = () => {
+  // 2. GA 초기화
+  useEffect(() => {
+    ReactGA.initialize(`${process.env.GA_MEASUREMENT_ID}`);
+  }, []);
+
+  // 5. GTM 초기화
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: `${process.env.GTM_ID}`,
+    });
+  }, []);
+
   return (
     <Router>
+      <TrackPageView /> {/* 3. 여기 최상단에 들어왔습니다. */}
       <div className='MainBox'>
         <Header />
         {/* <Button /> */}
