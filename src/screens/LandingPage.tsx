64adff8ce2bd87c98a9439544e8696fec3236fad
BoRamSon <onbrson1@gmail.com>
2025-01-27 03:31:18 +0900

    feat(screens): update LandingPage

--- src/screens/LandingPage.tsx
+++ src/screens/LandingPage.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import TagManager from 'react-gtm-module';
-import '../styles/LandingPage.css';
+import Row1 from '../components/LandingPage/Row1';
+import Row2 from '../components/LandingPage/Row2';
+import Row3 from '../components/LandingPage/Row3';
+import Row4 from '../components/LandingPage/Row4';
+import Row5 from '../components/LandingPage/Row5';
 import cover from '../assets/images/URLog_Cover_1x.png';
 import mainmap from '../assets/images/URLog_MainMap_1x.png';
 import album from '../assets/images/URLog_Gallary_1x.png';
@@ -8,12 +12,7 @@ import digitalalbum from '../assets/images/URLog_DigitalAlbum_1x.png';
 import camera from '../assets/images/URLog_Camera.png';
 import telescope from '../assets/images/image_Telescope.png';
 import bestshot from '../assets/images/URLog_DigitalAlbum_BestShot.png';
-import DownloadButton from '../components/DownloadButton';
-import Row1 from '../components/LandingPage/Row1';
-import Row2 from '../components/LandingPage/Row2';
-import Row3 from '../components/LandingPage/Row3';
-import Row4 from '../components/LandingPage/Row4';
-import Row5 from '../components/LandingPage/Row5';
+import DownloadButton from '../components/DownloadButton.tsx';
 
 const LandingPage: React.FC = () => {
   useEffect(() => {
@@ -26,6 +25,8 @@ const LandingPage: React.FC = () => {
 
   return (
     <>
+      {/* ========================================================================================================== */}
+
       <div className='relative isolate overflow-hidden bg-white px-6 py-10 lg:overflow-visible lg:px-0 mt-16'>
         {/* relative: position relative, isolate: 새로운 스태킹 컨텍스트 생성, overflow-hidden: 오버플로우 숨기기, bg-white: 배경색 흰색, px-6: x축 패딩 1.5rem, py-10: y축 패딩 2.5rem, 
         lg:overflow-visible: 큰 화면에서 오버플로우 보이기, lg:px-0: 큰 화면에서 x축 패딩 0, mt-16: 위쪽 마진 4rem */}
@@ -64,6 +65,8 @@ const LandingPage: React.FC = () => {
             />
           </svg>
         </div>
+
+        {/* ========================================================================================================== */}
 
         <div className='mx-auto grid max-w-2xl grid-cols-1 grid-row-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:grid-row-40 lg:items-start lg:gap-y-10'>
           {/* mx-auto: x축 마진 자동, grid: 그리드 디스플레이, max-w-2xl: 최대 너비 2xl, grid-cols-1: 1열, grid-row-1: 1행, gap-x-8: x축 간격 2rem, gap-y-16: y축 간격 4rem, lg:mx-0: 큰 화면에서 x축 마진 0, lg:max-w-none: 큰 화면에서 최대 너비 없음, lg:grid-cols-2: 큰 화면에서 2열, lg:grid-row-40: 큰 화면에서 40행, lg:items-start: 큰 화면에서 아이템 정렬 시작, lg:gap-y-10: 큰 화면에서 y축 간격 2.5rem */}
